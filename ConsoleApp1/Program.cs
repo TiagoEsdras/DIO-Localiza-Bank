4112976c1490d53f3162a76a89307a7b4d57f0d3
Tiago Esdras <tiagoesdrasvieira@gmail.com>
2021-02-08 18:57:37 -0300

    Iniciando Logica Opcao do Usuario

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,12 +1,52 @@
 ﻿using System;
 
-namespace ConsoleApp1
+namespace ConsoleBank
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string opcaoUsuario = ObterOpcaoUsuario();
+
+            while(opcaoUsuario != "X")
+            {
+                switch(opcaoUsuario)
+                {
+                    case "1":
+                        break;
+                    case "2":
+                        break;
+                    case "3":
+                        break;
+                    case "4":
+                        break;
+                    case "X":
+                        break;
+                    default:
+                        break;
+                }
+                opcaoUsuario = ObterOpcaoUsuario();
+            }
+
         }
+        private static string ObterOpcaoUsuario()
+        {
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("-------- Escolha uma opção --------");
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("1 - Cadastrar ---------------------");
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("2 - Depositar ---------------------");
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("3 - Sacar -------------------------");
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("4 - Transferir --------------------");
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("X - Sair --------------------------");
+            Console.WriteLine("-----------------------------------");
+
+            string opcaoUsuario = Console.ReadLine().ToUpper();
+            return opcaoUsuario;
+        }  
     }
 }
